facecdd21413edc9104f9a1ad6b208b4781bc1ff
Alexandre <alexandrebezerra3207@gmail.com>
2024-04-18 23:16:54 -0300

    creation of new struct(Local) and addition as parameter

--- file_operations.h
+++ file_operations.h
@@ -9,7 +9,12 @@ typedef struct serv{
     std::vector<int> job_indexes;
 }Serv;
 
+typedef struct local{
+    int local_cost = 0;
+    std::vector<int> job_indexes;
+}Local;
 
-void read_instance(std::string path, std::vector<Serv*> &servs, std::vector<std::vector<int>> &m_time, std::vector<std::vector<int>> & m_cost, int& local_cost);
+
+void read_instance(std::string path, std::vector<Serv*> &servs, std::vector<std::vector<int>> &m_time, std::vector<std::vector<int>> & m_cost, std::vector<Local*> &local);
 
 #endif
--- solver.cpp
+++ solver.cpp
@@ -5,10 +5,11 @@ int main(int argc, char* argv[]) {
     
     std::vector<Serv*> servs;
     std::vector<std::vector<int>> m_time, m_cost;
+    std::vector<Local*> local;
 
     std::string path = "test instances\\n5m15A.txt";
 
-    read_instance(path, servs, m_time, m_cost);
+    read_instance(path, servs, m_time, m_cost, local);
 
     return 0;
 }
