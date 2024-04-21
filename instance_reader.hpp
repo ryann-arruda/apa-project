7f4a9aac66921e7e370650d769cd0262eafcc84a
ryann-arruda <ryann.arruda_159@hotmail.com>
2024-04-20 23:54:18 -0300

    Adding data types file and renaming 'file_operations' to 'instance_reader'

---
+++ instance_reader.hpp
@@ -0,0 +1,10 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+#include <string>
+#include <vector>
+#include "data_types.hpp"
+
+void read_instance(std::string path, std::vector<Serv*> &servs, std::vector<std::vector<int>> &m_time, std::vector<std::vector<int>> & m_cost, Local &local);
+
+#endif
