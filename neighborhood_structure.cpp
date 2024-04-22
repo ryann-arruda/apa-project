cb68a0fd8a55c3d73cbf1a6fd88bf61727efd161
Alexandre <alexandrebezerra3207@gmail.com>
2024-04-22 00:39:49 -0300

    creation of the reinsertion neighborhood structure

--- neighborhood_structure.cpp
+++ neighborhood_structure.cpp
@@ -19,7 +19,6 @@ std::pair<std::vector<Serv*>*, Local*> exploreNeighborhood(int current, std::vec
 }
 
 std::pair<std::vector<Serv*>*, Local*> makeSwap(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution){
-    int cost = 0;
 
     std::default_random_engine generator;
     std::uniform_int_distribution<int> options(0,1);
@@ -72,6 +71,59 @@ std::pair<std::vector<Serv*>*, Local*> makeSwap(std::vector<std::vector<int>> &m
     if((*preSolution.first)[serv1]->capacity + m_time[serv1][job1] >= m_time[serv1][job2] && (*preSolution.first)[serv2]->capacity + m_time[serv2][job2] >= m_time[serv2][job1]){
         if(m_cost[serv1][job2] + m_cost[serv2][job1] < m_cost[serv1][job1] + m_cost[serv2][job2]){
             std::swap((*preSolution.first)[serv1]->job_indexes[job1], (*preSolution.first)[serv2]->job_indexes[job2]);
+        }
+    }
+
+    return preSolution;
+}
+
+std::pair<std::vector<Serv*>*, Local*> makeReinsertion(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution){
+    
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> options(0,1);
+
+    int result = options(generator);
+    int serv1;
+    int serv2;
+    int job1;
+
+    if(result == 0 ){
+        std::uniform_int_distribution<int> servs(0,preSolution.first->size() - 1);
+
+        serv1 = servs(generator);
+
+        std::uniform_int_distribution<int> jobs_serv1(0,(*preSolution.first)[serv1]->job_indexes.size() - 1);
+
+        job1 = jobs_serv1(generator);
+
+        result = options(generator);
+
+        if(result == 0){
+
+            serv2 = servs(generator);
+
+        }
+
+    }
+
+    else{
+
+        std::uniform_int_distribution<int> jobs_local(0,preSolution.second->job_indexes.size() - 1);
+
+        job1 = jobs_local(generator);
+
+        std::uniform_int_distribution<int> servs(0,preSolution.first->size() - 1);
+
+        serv1 = servs(generator);
+    }
+
+
+    if((*preSolution.first)[serv2]->capacity >= m_time[serv2][job1]){
+        if(m_cost[serv2][job1] < m_cost[serv1][job1]){
+            (*preSolution.first)[serv1]->job_indexes.erase((*preSolution.first)[serv1]->job_indexes.begin()+job1);
+            (*preSolution.first)[serv1]->capacity += m_time[serv1][job1];
+            (*preSolution.first)[serv2]->job_indexes.push_back(job1);
+            (*preSolution.first)[serv2]->capacity -= m_time[serv2][job1];
         }
     }
 
