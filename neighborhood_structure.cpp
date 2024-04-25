53d1c4bf888cbf8df44ea2d259b9ecf0d9b1ef3b
ryann-arruda <ryann.arruda_159@hotmail.com>
2024-04-25 00:33:20 -0300

    Changing the type of solution vector and the way it is accessed in neighborhood functions

--- neighborhood_structure.cpp
+++ neighborhood_structure.cpp
@@ -5,7 +5,7 @@
 #include <chrono>
 #include <limits>
 
-std::pair<std::vector<Serv*>*, Local*> exploreNeighborhood(int current, std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution){
+std::pair<std::vector<Serv>, Local> exploreNeighborhood(int current, std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution){
     
     switch(current){
         case 0:
@@ -19,24 +19,25 @@ std::pair<std::vector<Serv*>*, Local*> exploreNeighborhood(int current, std::vec
             break;
         
         default:
-            return std::make_pair(nullptr, nullptr);
+            std::pair<std::vector<Serv>, Local> aux;
+            return aux;
     }
 }
 
-//std::pair<std::vector<Serv*>*, Local*> preSolution
-std::pair<std::vector<Serv*>*, Local*> makeSwap(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution){
+//std::pair<std::vector<Serv>, Local> preSolution
+std::pair<std::vector<Serv>, Local> makeSwap(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution){
     int origin_serv = 0, origin_job = 0;
     int dest_serv = 0, dest_job = 0;
     int best_cost = std::numeric_limits<int>::max();
 
-    int numb_servs = preSolution.first -> size();
+    int numb_servs = preSolution.first.size();
     for(int serv1 = 0; serv1 < numb_servs - 1; serv1++){
-        for(int job1 = 0; job1 < (*preSolution.first)[serv1]->job_indexes.size(); job1++){
+        for(int job1 = 0; job1 < preSolution.first[serv1].job_indexes.size(); job1++){
 
             for(int serv2 = serv1 + 1; serv2 < numb_servs; serv2++){
-                for(int job2 = 0; job2 < (*preSolution.first)[serv2]->job_indexes.size(); job2++){
-                    int exec_time_serv1 = m_time[serv1][job1] + (*preSolution.first)[serv1]->capacity;
-                    int exec_time_serv2 = m_time[serv2][job2] + (*preSolution.first)[serv2]->capacity;
+                for(int job2 = 0; job2 < preSolution.first[serv2].job_indexes.size(); job2++){
+                    int exec_time_serv1 = m_time[serv1][job1] + preSolution.first[serv1].capacity;
+                    int exec_time_serv2 = m_time[serv2][job2] + preSolution.first[serv2].capacity;
 
                     if(exec_time_serv1 >= m_time[serv1][job2] && exec_time_serv2 >= m_time[serv2][job1]){
                         int exchange_cost = m_cost[serv1][job2] + m_cost[serv2][job1] - m_cost[serv1][job1] - m_cost[serv2][job2];
@@ -56,12 +57,12 @@ std::pair<std::vector<Serv*>*, Local*> makeSwap(std::vector<std::vector<int>> &m
     }
 
     for(int serv1 = 0; serv1 < numb_servs; serv1++){
-        for(int job1 = 0; job1 < (*preSolution.first)[serv1]->job_indexes.size(); job1++){
-            for(int job2 = 0; job2 < preSolution.second->job_indexes.size(); job2++){
-                int exec_time_serv1 = m_time[serv1][job1] + (*preSolution.first)[serv1]->capacity;
+        for(int job1 = 0; job1 < preSolution.first[serv1].job_indexes.size(); job1++){
+            for(int job2 = 0; job2 < preSolution.second.job_indexes.size(); job2++){
+                int exec_time_serv1 = m_time[serv1][job1] + preSolution.first[serv1].capacity;
 
-                int job_local = (*preSolution.second).job_indexes[job2];
-                int local_cost = preSolution.second->local_cost;
+                int job_local = preSolution.second.job_indexes[job2];
+                int local_cost = preSolution.second.local_cost;
                 if(exec_time_serv1 >= m_time[serv1][job_local]){
                     int exchange_cost = m_cost[serv1][job_local] - m_cost[serv1][job1];
 
@@ -79,17 +80,26 @@ std::pair<std::vector<Serv*>*, Local*> makeSwap(std::vector<std::vector<int>> &m
 
     if(best_cost != std::numeric_limits<int>::max()){
         if(dest_serv != -1){
-            std::swap((*preSolution.first)[origin_serv]->job_indexes[origin_job], (*preSolution.first)[dest_serv]->job_indexes[dest_job]);
+            preSolution.first[origin_serv].capacity += m_time[origin_serv][origin_job];
+            preSolution.first[dest_serv].capacity += m_time[dest_serv][dest_job];
+
+            preSolution.first[origin_serv].capacity -= m_time[origin_serv][dest_job];
+            preSolution.first[dest_serv].capacity -= m_time[dest_serv][origin_job];
+
+            std::swap(preSolution.first[origin_serv].job_indexes[origin_job], preSolution.first[dest_serv].job_indexes[dest_job]);
         }
         else{
-            std::swap((*preSolution.first)[origin_serv]->job_indexes[origin_job], preSolution.second->job_indexes[dest_job]);
+            preSolution.first[origin_serv].capacity += m_time[origin_serv][origin_job];
+            preSolution.first[origin_serv].capacity -= m_time[origin_serv][dest_job];
+            
+            std::swap(preSolution.first[origin_serv].job_indexes[origin_job], preSolution.second.job_indexes[dest_job]);
         }
     }
 
     return preSolution;
 }
 
-std::pair<std::vector<Serv*>*, Local*> makeReinsertion(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution){
+std::pair<std::vector<Serv>, Local> makeReinsertion(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution){
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     std::uniform_int_distribution<int> options(0,1);
@@ -100,11 +110,11 @@ std::pair<std::vector<Serv*>*, Local*> makeReinsertion(std::vector<std::vector<i
     int job1;
 
     if(result == 0 ){
-        std::uniform_int_distribution<int> servs(0,preSolution.first->size() - 1);
+        std::uniform_int_distribution<int> servs(0,preSolution.first.size() - 1);
 
         serv1 = servs(generator);
 
-        std::uniform_int_distribution<int> jobs_serv1(0,(*preSolution.first)[serv1]->job_indexes.size() - 1);
+        std::uniform_int_distribution<int> jobs_serv1(0,preSolution.first[serv1].job_indexes.size() - 1);
 
         job1 = jobs_serv1(generator);
 
@@ -120,56 +130,56 @@ std::pair<std::vector<Serv*>*, Local*> makeReinsertion(std::vector<std::vector<i
 
     else{
 
-        std::uniform_int_distribution<int> jobs_local(0,preSolution.second->job_indexes.size() - 1);
+        std::uniform_int_distribution<int> jobs_local(0,preSolution.second.job_indexes.size() - 1);
 
         job1 = jobs_local(generator);
 
-        std::uniform_int_distribution<int> servs(0,preSolution.first->size() - 1);
+        std::uniform_int_distribution<int> servs(0,preSolution.first.size() - 1);
 
         serv1 = servs(generator);
     }
 
 
-    if((*preSolution.first)[serv2]->capacity >= m_time[serv2][job1]){
+    if(preSolution.first[serv2].capacity >= m_time[serv2][job1]){
         if(m_cost[serv2][job1] < m_cost[serv1][job1]){
-            (*preSolution.first)[serv2]->job_indexes.push_back((*preSolution.first)[serv1]->job_indexes[job1]);
-            (*preSolution.first)[serv1]->job_indexes.erase((*preSolution.first)[serv1]->job_indexes.begin()+job1);
-            (*preSolution.first)[serv1]->capacity += m_time[serv1][job1];
-            (*preSolution.first)[serv2]->capacity -= m_time[serv2][job1];
+            preSolution.first[serv2].job_indexes.push_back(preSolution.first[serv1].job_indexes[job1]);
+            preSolution.first[serv1].job_indexes.erase(preSolution.first[serv1].job_indexes.begin()+job1);
+            preSolution.first[serv1].capacity += m_time[serv1][job1];
+            preSolution.first[serv2].capacity -= m_time[serv2][job1];
         }
     }
 
     return preSolution;
 }
 
-std::pair<std::vector<Serv*>*, Local*> makeModifiedEjectionChain(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution){
+std::pair<std::vector<Serv>, Local> makeModifiedEjectionChain(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution){
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     int job;
 
-    for(int serv = 0; serv < preSolution.first -> size() - 1; serv++){
-        std::uniform_int_distribution<int> jobs_serv(0,(*preSolution.first)[serv]->job_indexes.size() - 1);
+    for(int serv = 0; serv < preSolution.first.size() - 1; serv++){
+        std::uniform_int_distribution<int> jobs_serv(0,preSolution.first[serv].job_indexes.size() - 1);
 
         job = jobs_serv(generator);
 
-        if((*preSolution.first)[serv + 1] -> capacity >= m_time[serv + 1][job]){
+        if(preSolution.first[serv + 1].capacity >= m_time[serv + 1][job]){
             if(m_cost[serv + 1][job] < m_cost[serv][job]){
-                (*preSolution.first)[serv]->job_indexes.erase((*preSolution.first)[serv]->job_indexes.begin()+job);
-                (*preSolution.first)[serv]->capacity += m_time[serv][job];
-                (*preSolution.first)[serv + 1]->job_indexes.push_back(job);
-                (*preSolution.first)[serv + 1]->capacity -= m_time[serv][job];
+                preSolution.first[serv].job_indexes.erase(preSolution.first[serv].job_indexes.begin()+job);
+                preSolution.first[serv].capacity += m_time[serv][job];
+                preSolution.first[serv + 1].job_indexes.push_back(job);
+                preSolution.first[serv + 1].capacity -= m_time[serv][job];
             }
         }
     }
 
-    int last_serv = preSolution.first -> size() - 1;
-    std::uniform_int_distribution<int> jobs_serv(0,(*preSolution.first)[last_serv]->job_indexes.size() - 1);
+    int last_serv = preSolution.first.size() - 1;
+    std::uniform_int_distribution<int> jobs_serv(0,preSolution.first[last_serv].job_indexes.size() - 1);
     job = jobs_serv(generator);
 
-    if(preSolution.second -> local_cost < m_cost[last_serv][job]){
-        preSolution.second -> job_indexes.push_back((*preSolution.first)[last_serv]->job_indexes[job]);
-        (*preSolution.first)[last_serv]->job_indexes.erase((*preSolution.first)[last_serv]->job_indexes.begin()+job);
-        (*preSolution.first)[last_serv]->capacity += m_time[last_serv][job];
+    if(preSolution.second.local_cost < m_cost[last_serv][job]){
+        preSolution.second.job_indexes.push_back(preSolution.first[last_serv].job_indexes[job]);
+        preSolution.first[last_serv].job_indexes.erase(preSolution.first[last_serv].job_indexes.begin()+job);
+        preSolution.first[last_serv].capacity += m_time[last_serv][job];
     }
 
     return preSolution;
--- neighborhood_structure.hpp
+++ neighborhood_structure.hpp
@@ -5,9 +5,9 @@
 #include <vector>
 #include "data_types.hpp"
 
-std::pair<std::vector<Serv*>*, Local*> exploreNeighborhood(int current, std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution);
-std::pair<std::vector<Serv*>*, Local*> makeSwap(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution);
-std::pair<std::vector<Serv*>*, Local*> makeReinsertion(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution);
-std::pair<std::vector<Serv*>*, Local*> makeModifiedEjectionChain(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv*>*, Local*> preSolution);
+std::pair<std::vector<Serv>, Local> exploreNeighborhood(int current, std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution);
+std::pair<std::vector<Serv>, Local> makeSwap(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution);
+std::pair<std::vector< Serv>, Local> makeReinsertion(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector< Serv>, Local> preSolution);
+std::pair<std::vector< Serv>, Local> makeModifiedEjectionChain(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector< Serv>, Local> preSolution);
 
 #endif
