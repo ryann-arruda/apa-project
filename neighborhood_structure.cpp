5d09d1e9f30a720bf43e16eed16c5eac70ec535c
ryann-arruda <ryann.arruda_159@hotmail.com>
2024-04-25 13:07:35 -0300

    Correction of swap and reinsertion neighborhood structures

--- neighborhood_structure.cpp
+++ neighborhood_structure.cpp
@@ -9,14 +9,14 @@ std::pair<std::vector<Serv>, Local> exploreNeighborhood(int current, std::vector
     
     switch(current){
         case 0:
-            return makeSwap(m_cost, m_time, preSolution);
-            break;
-        case 1:
             return makeReinsertion(m_cost, m_time, preSolution);
             break;
-        case 2:
-            return makeModifiedEjectionChain(m_cost, m_time, preSolution);
+        case 1:
+            return makeSwap(m_cost, m_time, preSolution);
             break;
+        /*case 2:
+            return makeModifiedEjectionChain(m_cost, m_time, preSolution);
+            break;*/
         
         default:
             std::pair<std::vector<Serv>, Local> aux;
@@ -33,14 +33,17 @@ std::pair<std::vector<Serv>, Local> makeSwap(std::vector<std::vector<int>> &m_co
     int numb_servs = preSolution.first.size();
     for(int serv1 = 0; serv1 < numb_servs - 1; serv1++){
         for(int job1 = 0; job1 < preSolution.first[serv1].job_indexes.size(); job1++){
+            int job1_index = preSolution.first[serv1].job_indexes[job1];
 
             for(int serv2 = serv1 + 1; serv2 < numb_servs; serv2++){
                 for(int job2 = 0; job2 < preSolution.first[serv2].job_indexes.size(); job2++){
-                    int exec_time_serv1 = m_time[serv1][job1] + preSolution.first[serv1].capacity;
-                    int exec_time_serv2 = m_time[serv2][job2] + preSolution.first[serv2].capacity;
+                    int job2_index = preSolution.first[serv2].job_indexes[job2];
 
-                    if(exec_time_serv1 >= m_time[serv1][job2] && exec_time_serv2 >= m_time[serv2][job1]){
-                        int exchange_cost = m_cost[serv1][job2] + m_cost[serv2][job1] - m_cost[serv1][job1] - m_cost[serv2][job2];
+                    int exec_time_serv1 = m_time[serv1][job1_index] + preSolution.first[serv1].capacity;
+                    int exec_time_serv2 = m_time[serv2][job2_index] + preSolution.first[serv2].capacity;
+
+                    if(exec_time_serv1 >= m_time[serv1][job2_index] && exec_time_serv2 >= m_time[serv2][job1_index]){
+                        int exchange_cost = m_cost[serv1][job2_index] + m_cost[serv2][job1_index] - m_cost[serv1][job1_index] - m_cost[serv2][job2_index];
 
                         if(exchange_cost < best_cost){
                             best_cost = exchange_cost;
@@ -58,13 +61,16 @@ std::pair<std::vector<Serv>, Local> makeSwap(std::vector<std::vector<int>> &m_co
 
     for(int serv1 = 0; serv1 < numb_servs; serv1++){
         for(int job1 = 0; job1 < preSolution.first[serv1].job_indexes.size(); job1++){
-            for(int job2 = 0; job2 < preSolution.second.job_indexes.size(); job2++){
-                int exec_time_serv1 = m_time[serv1][job1] + preSolution.first[serv1].capacity;
+            int job1_index = preSolution.first[serv1].job_indexes[job1];
 
-                int job_local = preSolution.second.job_indexes[job2];
+            for(int job2 = 0; job2 < preSolution.second.job_indexes.size(); job2++){
+                int exec_time_serv1 = m_time[serv1][job1_index] + preSolution.first[serv1].capacity;
+
+                int job2_index = preSolution.second.job_indexes[job2];
+
                 int local_cost = preSolution.second.local_cost;
-                if(exec_time_serv1 >= m_time[serv1][job_local]){
-                    int exchange_cost = m_cost[serv1][job_local] - m_cost[serv1][job1];
+                if(exec_time_serv1 >= m_time[serv1][job2_index]){
+                    int exchange_cost = m_cost[serv1][job2_index] - m_cost[serv1][job1_index];
 
                     if(exchange_cost < best_cost){
                         best_cost = exchange_cost;
@@ -79,18 +85,24 @@ std::pair<std::vector<Serv>, Local> makeSwap(std::vector<std::vector<int>> &m_co
     }
 
     if(best_cost != std::numeric_limits<int>::max()){
-        if(dest_serv != -1){
-            preSolution.first[origin_serv].capacity += m_time[origin_serv][origin_job];
-            preSolution.first[dest_serv].capacity += m_time[dest_serv][dest_job];
+        int job1_index = preSolution.first[origin_serv].job_indexes[origin_job];
 
-            preSolution.first[origin_serv].capacity -= m_time[origin_serv][dest_job];
-            preSolution.first[dest_serv].capacity -= m_time[dest_serv][origin_job];
+        if(dest_serv != -1){
+            int job2_index = preSolution.first[dest_serv].job_indexes[dest_job];
+
+            preSolution.first[origin_serv].capacity += m_time[origin_serv][job1_index];
+            preSolution.first[dest_serv].capacity += m_time[dest_serv][job2_index];
+
+            preSolution.first[origin_serv].capacity -= m_time[origin_serv][job2_index];
+            preSolution.first[dest_serv].capacity -= m_time[dest_serv][job1_index];
 
             std::swap(preSolution.first[origin_serv].job_indexes[origin_job], preSolution.first[dest_serv].job_indexes[dest_job]);
         }
         else{
-            preSolution.first[origin_serv].capacity += m_time[origin_serv][origin_job];
-            preSolution.first[origin_serv].capacity -= m_time[origin_serv][dest_job];
+            int job2_index = preSolution.second.job_indexes[dest_job];
+
+            preSolution.first[origin_serv].capacity += m_time[origin_serv][job1_index];
+            preSolution.first[origin_serv].capacity -= m_time[origin_serv][job2_index];
             
             std::swap(preSolution.first[origin_serv].job_indexes[origin_job], preSolution.second.job_indexes[dest_job]);
         }
@@ -100,45 +112,85 @@ std::pair<std::vector<Serv>, Local> makeSwap(std::vector<std::vector<int>> &m_co
 }
 
 std::pair<std::vector<Serv>, Local> makeReinsertion(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> preSolution){
-    std::vector<Serv> servs = preSolution.first;
-    Local local = preSolution.second;
+    std::vector<Serv> &servs = preSolution.first;
+    Local &local = preSolution.second;
 
     int origin_serv = 0, origin_job = 0;
     int dest_serv = 0;
     int best_cost = std::numeric_limits<int>::max();
 
-    for(int serv1 = 0; serv1 < servs.size()-1; serv1++){
+    for(int serv1 = 0; serv1 < servs.size(); serv1++){
         for(int job = 0; job < servs[serv1].job_indexes.size(); job++){
+            int job_index = servs[serv1].job_indexes[job];
+            
+            int cost = local.local_cost - m_cost[serv1][job_index];
+
+            if(cost < best_cost){
+                best_cost = cost;
+                origin_serv = serv1;
+                dest_serv = -1;
+                origin_job = job;
+            }
+
             for(int serv2 = 0; serv2 < servs.size(); serv2++){
                 if(serv1 == serv2){
                     continue;
                 }
 
-                if(servs[serv2].capacity >= m_time[serv2][job]){
-                    int cost = m_cost[serv2][job] - m_cost[serv1][job];
+                if(servs[serv2].capacity >= m_time[serv2][job_index]){
+                    cost = m_cost[serv2][job_index] - m_cost[serv1][job_index];
+
                     if(cost < best_cost){
                         best_cost = cost;
                         origin_serv = serv1;
                         dest_serv = serv2;
                         origin_job = job;
                     }
-
                 }
 
             }
         }
     }
-    
-    int last_server = servs.size()-1;
 
-    for(int job = 0; job < servs[last_server].job_indexes.size(); job++){
-        int cost = local.local_cost - m_cost[last_server][job];
-        if(cost < best_cost){
-            best_cost = cost;
-            origin_serv = last_server;
-            dest_serv = -1;
-            origin_job = job;
+    for(int job = 0; job < local.job_indexes.size(); job++){
+        for(int serv1 = 0; serv1 < servs.size(); serv1++){
+            int local_job_index = local.job_indexes[job];
+
+            if(servs[serv1].capacity >= m_time[serv1][local_job_index]){
+                int cost = m_cost[serv1][local_job_index] - local.local_cost;
+                
+                if(cost < best_cost){
+                    best_cost = cost;
+                    origin_serv = -1;
+                    dest_serv = serv1;
+                    origin_job = job;
+                }
+            }
         }
+    }
+
+    if(origin_serv != -1){
+        int job_index = servs[origin_serv].job_indexes[origin_job];
+        servs[origin_serv].capacity += m_time[origin_serv][job_index];
+
+        if(dest_serv != -1){
+            servs[dest_serv].capacity -= m_time[dest_serv][job_index];
+
+            servs[origin_serv].job_indexes.erase(servs[origin_serv].job_indexes.begin()+origin_job);
+            servs[dest_serv].job_indexes.push_back(job_index);
+        }
+        else{
+            servs[origin_serv].job_indexes.erase(servs[origin_serv].job_indexes.begin()+origin_job);
+            local.job_indexes.push_back(job_index);
+        }
+    }
+    else{
+        int job_index = local.job_indexes[origin_job];
+
+        servs[dest_serv].capacity -= m_time[dest_serv][job_index];
+
+        local.job_indexes.erase(local.job_indexes.begin()+origin_job);
+        servs[dest_serv].job_indexes.push_back(job_index);
     }
 
     return preSolution;
