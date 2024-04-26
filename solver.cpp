afce9f32318b1d2865e8362df6291eff5951cefa
ryann-arruda <ryann.arruda_159@hotmail.com>
2024-04-26 18:20:24 -0300

    Creation of the function to write to the file and timekeeping

--- solver.cpp
+++ solver.cpp
@@ -3,10 +3,14 @@
 #include "neighborhood_structure.hpp"
 #include <iostream>
 #include <limits>
+#include <fstream>
+#include <chrono>
+#include <thread>
 
 void greedy_algorithm(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::vector<Serv> &servs, Local& local, std::pair<std::vector<Serv>, Local> &solution);
 int objective_function(std::vector<std::vector<int>> &m_cost, std::pair<std::vector<Serv>, Local> &solution);
 std::pair<std::vector<Serv>, Local> vnd(int n_neighborhood_structure, std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::pair<std::vector<Serv>, Local> solution);
+void make_file(std::string filename, std::vector<std::vector<int>> &m_cost, std::pair<std::vector<Serv>, Local> solution);
 
 int main(int argc, char* argv[]) {
     std::vector<Serv> servs;
@@ -19,48 +23,22 @@ int main(int argc, char* argv[]) {
 
     std::pair<std::vector<Serv>, Local> solution;
 
+    auto start = std::chrono::high_resolution_clock::now();
     greedy_algorithm(m_cost, m_time, servs, local, solution);
+    auto end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Objective function (before): " << objective_function(m_cost, solution) << std::endl;
-    std::cout << "Servidores: " << std::endl;
-    for(int i = 0; i < solution.first.size(); i++){
-        for(int j = 0; j < solution.first[i].job_indexes.size(); j++){
-            std::cout << solution.first[i].job_indexes[j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-    std::cout << std::endl;
+    std::cout << "Tempo para a Heurística Construtiva: " << duration.count() << "ms" << std::endl;
 
-    std::cout << "Local: " << std::endl;
-    for(int i = 0; i < solution.second.job_indexes.size(); i++){
-        std::cout << solution.second.job_indexes[i] << " ";
-    }
-    std::cout << std::endl;
+    start = std::chrono::high_resolution_clock::now();
+    solution = vnd(3, m_cost, m_time, solution);
+    end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Depois do vnd: " << "\n\n";
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Tempo para o VND: " << duration.count() << "ms" << std::endl;
 
-    solution = vnd(2, m_cost, m_time, solution);
-    //solution = exploreNeighborhood(0, m_cost, m_time, solution);
-    std::cout << "Objective function (after): " << objective_function(m_cost, solution);
-    std::cout << std::endl;
-
-    std::cout << "Servidores: " << std::endl;
-    for(int i = 0; i < solution.first.size(); i++){
-        for(int j = 0; j < solution.first[i].job_indexes.size(); j++){
-            std::cout << solution.first[i].job_indexes[j] << " ";
-        }
-        std::cout << " # Capacidade do servidor(" << i << "): " << solution.first[i].capacity<< std::endl;
-    }
-
-    std::cout << std::endl;
-
-    std::cout << "Local: " << std::endl;
-    for(int i = 0; i < solution.second.job_indexes.size(); i++){
-        std::cout << solution.second.job_indexes[i] << " ";
-    }
-    std::cout << std::endl;
-
+    make_file("result.txt", m_cost, solution);
 
     return 0;
 }
@@ -128,7 +106,7 @@ std::pair<std::vector<Serv>, Local> vnd(int n_neighborhood_structure, std::vecto
 
     while(k < n_neighborhood_structure){
         std::pair<std::vector<Serv>, Local> aux = exploreNeighborhood(k, m_cost, m_time, bestSolution);
-        std::cout << "K: " << k << std::endl;
+
         int firstCost = objective_function(m_cost, bestSolution);
         int secondCost = objective_function(m_cost, aux);
 
@@ -142,4 +120,47 @@ std::pair<std::vector<Serv>, Local> vnd(int n_neighborhood_structure, std::vecto
     }
 
     return bestSolution;
+}
+
+int servers_cost(std::vector<std::vector<int>> &m_cost, std::pair<std::vector<Serv>, Local> solution){
+    std::vector<Serv> &servs = solution.first;
+    int cost = 0;
+
+    for(int serv = 0; serv < servs.size(); serv++){
+        for(int job = 0; job < servs[serv].job_indexes.size(); job++){
+            cost += m_cost[serv][servs[serv].job_indexes[job]];
+        }
+    }
+
+    return cost;
+}
+
+void make_file(std::string filename, std::vector<std::vector<int>> &m_cost, std::pair<std::vector<Serv>, Local> solution){
+    std::vector<Serv> &servs = solution.first;
+    Local &local = solution.second;
+    std::ofstream file(filename);
+
+    if(file.is_open()){
+        file << objective_function(m_cost, solution) << "\n";
+        file << servers_cost(m_cost, solution) << "\n";
+        file << local.job_indexes.size() * local.local_cost;
+
+        file << "\n\n";
+
+        for(int serv = 0; serv < servs.size(); serv++){
+            std::string line = "";
+
+            for(int job = 0; job < servs[serv].job_indexes.size(); job++){
+                line += std::to_string(servs[serv].job_indexes[job]);
+                line += " ";
+            }
+
+            file << line + "\n";
+        }
+
+        file.close();
+    }
+    else{
+        std::cerr << "Erro ao criar o arquivo de saída\n\n";
+    }
 }
