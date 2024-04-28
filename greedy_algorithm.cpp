f1f909b468c57289a1254f374794bef65ab9df0e
ryann-arruda <ryann.arruda_159@hotmail.com>
2024-04-28 13:27:50 -0300

    Removed the unused parameter m_cost and changed the name and type of variables
    The variable cost had its name changed to time and its type was changed from double to int

--- greedy_algorithm.cpp
+++ greedy_algorithm.cpp
@@ -16,29 +16,29 @@ bool check_chosen_servers(std::vector<int> &check_servs){
     return answer;
 }
 
-void greedy_algorithm(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::vector<Serv> servs, Local local, std::pair<std::vector<Serv>, Local> &solution){
+void greedy_algorithm(std::vector<std::vector<int>> &m_time, std::vector<Serv> servs, Local local, std::pair<std::vector<Serv>, Local> &solution){
     std::vector<int> jobs;
     std::vector<int> check_servs;
 
-    for(int i = 0; i < m_cost.size(); i++){
+    for(int i = 0; i < m_time.size(); i++){
         check_servs.push_back(0);
     }
 
-    for(int i = 0; i < m_cost[0].size(); i++){
+    for(int i = 0; i < m_time[0].size(); i++){
         jobs.push_back(i);
     }
 
     while(check_chosen_servers(check_servs)){
 
-        for(int serv = 0; serv < m_cost.size(); serv++){
-            double best = std::numeric_limits<double>::max();
+        for(int serv = 0; serv < m_time.size(); serv++){
+            int best = std::numeric_limits<int>::max();
             int job_index = -1;
 
             for(int job = 0; job < jobs.size(); job++){
-                double cost = m_time[serv][jobs[job]];
+                int time = m_time[serv][jobs[job]];
 
-                if(cost < best && m_time[serv][jobs[job]] <= servs[serv].capacity){
-                    best = cost;
+                if(time < best && m_time[serv][jobs[job]] <= servs[serv].capacity){
+                    best = time;
                     job_index = job;
                 }
             }
--- greedy_algorithm.hpp
+++ greedy_algorithm.hpp
@@ -6,6 +6,6 @@
 #include "data_types.hpp"
 
 bool check_chosen_servers(std::vector<int> &check_servs);
-void greedy_algorithm(std::vector<std::vector<int>> &m_cost, std::vector<std::vector<int>> &m_time, std::vector<Serv> servs, Local local, std::pair<std::vector<Serv>, Local> &solution);
+void greedy_algorithm(std::vector<std::vector<int>> &m_time, std::vector<Serv> servs, Local local, std::pair<std::vector<Serv>, Local> &solution);
 
 #endif
